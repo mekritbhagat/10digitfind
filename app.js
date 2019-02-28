b9cc296fb2e5497983dfd4b27dd3063a7d30d7cd
mekritbhagat <34103079+mekritbhagat@users.noreply.github.com>
2019-02-28 11:54:22 +0545

    more commits

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var multer = require('multer');
 var bodyparser = require('body-parser');
 
 var app = express();
@@ -8,15 +7,16 @@ var app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-
+//app.use(express.static('public'));
 app.get('/', (req, res) => {
-    var dataString = "Hello, 1234567890 , I 123 am 1234 from 12345 n 123456 conto 1234567 or 12345678 final 0006543210 111111 999990 00000";
-    var ck = /\d{10}/;
-    var matches = dataString.match(ck);
-    for (var i = 0; i < matches.length; i++) {
-        res.send( matches + ": is Matched" );
-    }
+    res.send(newText);
+});
+
+var text = "Hello, 123456789 , I 123 am 1234 from 12345 n 123456 conto 1234567 or 12345678 final 0006543210 111111 999990 00000";
+
+var newText = text.match(/\d{10}/,function(num){
+    newText = newText.join(' ');
+    return num;
 });
 
 app.listen(3000, () =>
